14ace841c7f6fe7dfaf4553e0eb9f91d544e6af2
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2024-05-26 21:21:05 +0100

    cold changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), icon()],
+  integrations: [
+    tailwind(),
+    icon({
+      iconDirectory: 'src/icons', // Ensure this path is correct based on your project structure
+    })
+  ],
 });
