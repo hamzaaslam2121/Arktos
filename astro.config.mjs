e7eed2fd3e53c889b278b4a8155529a3f6d863a8
Aritra <axs2157@student.bham.ac.uk>
2024-08-09 09:48:34 +0100

    Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [
+    clerk(),
     tailwind(),
     icon({
       iconDirectory: 'src/icons', // Ensure this path is correct based on your project structure
     })
   ],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
   vite: {
     server: {
       proxy: {
