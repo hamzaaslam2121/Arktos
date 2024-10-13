fb5558bf2a5bb50552ec27b6c455263aed3e9684
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2024-10-13 14:40:30 +0100

    test no clerk from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-import clerk from "@clerk/astro";
 // https://astro.build/config
 export default defineConfig({
   site: "https://arknetcouriers.co.uk",
   integrations: [
-    clerk(),
     tailwind(),
     icon({
       iconDirectory: 'src/icons', // Ensure this path is correct based on your project structure
