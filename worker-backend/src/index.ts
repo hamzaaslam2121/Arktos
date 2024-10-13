dfcf955eacdaba53c0ccaf3e22a7852de33c9ca5
Aritra <axs2157@student.bham.ac.uk>
2024-10-13 11:21:07 +0100

    clerk

--- worker-backend/src/index.ts
+++ worker-backend/src/index.ts
@@ -1,7 +1,4 @@
-addEventListener('fetch', event => {
-	event.respondWith(handleRequest(event.request))
-  })
-  
+
   async function handleRequest(request: Request): Promise<Response> {
 	const url = new URL(request.url)
 	
