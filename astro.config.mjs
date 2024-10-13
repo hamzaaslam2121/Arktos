008d5fff86b3ed569981da339a0466aaba92f43f
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2024-10-13 14:31:43 +0100

    domain test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,25 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 import clerk from "@clerk/astro";
-
+// https://astro.build/config
 export default defineConfig({
   site: "https://arknetcouriers.co.uk",
   integrations: [
     clerk(),
     tailwind(),
     icon({
-      iconDirectory: 'src/icons',
+      iconDirectory: 'src/icons', // Ensure this path is correct based on your project structure
     })
   ],
+  adapter: node({ mode: "standalone" }),
   output: "server",
-  adapter: cloudflare({ mode: "directory" }),
+  vite: {
+    server: {
+      proxy: {
+        '/api': 'worker-backend.hamzaaslam2121.workers.dev', // Proxy API requests to the Cloudflare Worker
+      },
+    },
+  },
 });
