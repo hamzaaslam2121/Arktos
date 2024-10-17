96bcecd6901c72dd2b65e4b99051775655d095e6
Aritra <axs2157@student.bham.ac.uk>
2024-10-17 20:27:23 +0100

    astro config from local to deployed version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://arknetcouriers.co.uk/",
   integrations: [
     clerk(),
     tailwind(),
     icon({
-      iconDirectory: 'src/icons', // Ensure this path is correct based on your project structure
+      iconDirectory: 'src/icons',
     })
   ],
-  adapter: node({ mode: "standalone" }),
-  output: "server",
   vite: {
+    define: {
+      'import.meta.env.PUBLIC_WORKER_URL': JSON.stringify(process.env.PUBLIC_WORKER_URL || 'https://worker-backend.hamzaaslam2121.workers.dev'),
+    },
     server: {
       proxy: {
-        '/api': 'http://localhost:8787', // Proxy API requests to the Cloudflare Worker
+        '/api': {
+          target: 'https://worker-backend.hamzaaslam2121.workers.dev',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
   },
